51eff5d55eb19fc36a6e3b3fcde3287b4c42dd14
stephen <osstephen70@gmail.com>
2025-10-06 12:58:28 +0100

    Add Hero section with PulseDot animation and update Home page layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,13 @@
+import Hero from '../components/sections/Hero';
+import Work from '../components/sections/Work';
+import Divider from '../components/ui/divider';
+
 export default function Home() {
-  return <div>Portfolio</div>;
+  return (
+    <div className='max-w-screen mx-auto h-screen px-24 border-stone-200 rounded-t-md'>
+      <Hero />
+      <Divider />
+      <Work />
+    </div>
+  );
 }

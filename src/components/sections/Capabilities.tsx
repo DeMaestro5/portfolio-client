cafa3070d4ba4b64e954e22a267834f1af571ac4
stephen <osstephen70@gmail.com>
2025-10-07 13:05:44 +0100

    Add Capabilities section to Home page and include additional Divider for improved layout

---
+++ src/components/sections/Capabilities.tsx
@@ -0,0 +1,10 @@
+import CapabilitiesContainer from '../ui/capabilitiesContainer';
+import Heading from '../ui/heading';
+export default function Capabilities() {
+  return (
+    <section className='relative py-[160px]'>
+      <Heading number='03' title='Capabilities' />
+      <CapabilitiesContainer />
+    </section>
+  );
+}

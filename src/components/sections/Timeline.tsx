9df2041d1608d0e00a602c7ac0392569321cbb88
stephen <osstephen70@gmail.com>
2025-10-21 09:17:34 +0100

    Enhance Timeline component to support 'show more' functionality, allowing users to toggle between viewing a limited set of events or all events. Update state management to track visibility and adjust rendering logic accordingly.

--- src/components/sections/Timeline.tsx
+++ src/components/sections/Timeline.tsx
@@ -1,11 +1,12 @@
 import { useMetrics } from '../../context/metrics/useMetrics';
 import TimelineCard from '../ui/timelineCard';
 import Heading from '../ui/heading';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Loader from '../ui/loader';
 import ErrorState from '../ui/error';
 
 export default function Activities() {
+  const [showAll, setShowAll] = useState(false);
   const { state, fetchTimeline } = useMetrics();
   const { loading, data, error } = state.timeline;
 
@@ -18,17 +19,27 @@ export default function Activities() {
   if (error)
     return <ErrorState message={error} onRetry={() => fetchTimeline(true)} />;
 
-  const first20 = data.events.slice(0, 20);
+  const visibleCount = showAll ? 20 : 4;
+  const visibleItems = data.events.slice(0, visibleCount);
 
+  function handleShowMore() {
+    setShowAll(!showAll);
+  }
   return (
     <section
       id='timeline'
       className='relative py-12 sm:py-16 md:py-20 lg:py-24'
     >
       <Heading number='02' title='Timeline' />
-      {first20.map((item, index) => (
+      {visibleItems.map((item, index) => (
         <TimelineCard key={`${item.date}-${index}`} timeline={item} />
       ))}
+      <button
+        className='text-sm text-neutral-500 border border-neutral-200 mt-4 p-2 rounded-full cursor-pointer hover:shadow'
+        onClick={handleShowMore}
+      >
+        {showAll ? 'show less timeline' : 'show more timeline'}
+      </button>
     </section>
   );
 }
